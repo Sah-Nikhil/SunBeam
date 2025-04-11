9fd9737f56e2397f2540ce50f5c033b8fc930eb9
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-04-11 12:35:01 +0530

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,18 +2,8 @@ import axios from 'axios'
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
 
-export default const api = axios.create({
-  baseURL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-
-
-// lib/generated/axios.ts
-
 const api = axios.create({
-  baseURL: '/api',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
